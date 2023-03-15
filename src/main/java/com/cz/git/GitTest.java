c06a5d39680b04f1d6fe00413d630637bd506aa5
Czy <Czy>
2023-03-15 22:54:53 +0800

    first commit

---
+++ src/main/java/com/cz/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.cz.git;
+
+/**
+ * @author czystart
+ * @create 2023-03-15 22:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
