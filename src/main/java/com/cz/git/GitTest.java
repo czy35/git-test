bc287c2a55b8d0472c72eb5573c576126e3a740d
Czy <Czy>
2023-03-15 23:08:24 +0800

    master commit

--- src/main/java/com/cz/git/GitTest.java
+++ src/main/java/com/cz/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!1");
         System.out.println("hello git!2");
+        System.out.println("master git!2");
     }
 
 }
