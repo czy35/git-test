894500aaf2d36601a42787630d0fb247e6c2ba8e
Czy <Czy>
2023-03-15 22:58:48 +0800

    second commit

--- src/main/java/com/cz/git/GitTest.java
+++ src/main/java/com/cz/git/GitTest.java
@@ -7,6 +7,7 @@ package com.cz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!1");
     }
 
 }
