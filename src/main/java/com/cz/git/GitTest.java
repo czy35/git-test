9c649494a4aad1d2dfda666f1c5465abf4e1dc03
Czy <Czy>
2023-03-15 23:27:12 +0800

    push commit

--- src/main/java/com/cz/git/GitTest.java
+++ src/main/java/com/cz/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello git!2");
         System.out.println("master git!2");
         System.out.println("hot-fix git!2");
+        System.out.println("push git!2");
     }
 
 }
