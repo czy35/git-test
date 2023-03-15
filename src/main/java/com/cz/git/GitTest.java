01d6faf1fb9204444d970cb0c914c19dcc087150
czy35 <127970319+czy35@users.noreply.github.com>
2023-03-15 23:45:00 +0800

    pull commit

--- src/main/java/com/cz/git/GitTest.java
+++ src/main/java/com/cz/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("master git!2");
         System.out.println("hot-fix git!2");
         System.out.println("push git!2");
+        System.out.println("pull git!2");
     }
 
 }
